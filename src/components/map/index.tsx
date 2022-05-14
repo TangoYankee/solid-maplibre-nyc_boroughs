21dc9af08741bdfc407ced3f30cfbdd482059ee4
timothy <miller.tim108@gmail.com>
2022-05-14 15:38:23 -0400

    remove zoom from state

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -6,16 +6,15 @@ import styles from './index.module.css';
 export const BoxedMap: Component = () => {
   let map: Map;
   let mapContainer: HTMLDivElement;
-  const [zoom, setZoom] = createSignal(9);
 
   onMount(() => {
     map = new maplibregl.Map({
       container: mapContainer,
       center: [-74.00, 40.68],
       style: `https://api.maptiler.com/maps/basic/style.json?key=${import.meta.env.VITE_MAP_TILER_KEY}`,
-      zoom: zoom(),
+      zoom: 9, 
     });
-    map.on('move', () => setZoom(Number.parseFloat(map.getZoom().toFixed(2))));
+
     map.on('load', () => {
       fetch('./data/new-york-city-boroughs.geojson')
       .then(response => response.json())
@@ -34,24 +33,9 @@ export const BoxedMap: Component = () => {
     });
   });
 
-  const changeZoom = (event: Event) => {
-    const target = event.target as HTMLInputElement;
-    const nextZoom = Number.parseFloat(target.value);
-    if(Number.isNaN(nextZoom)) {
-      console.warn('please enter decimal number for zoom');
-      return;
-    }
-    map.setZoom(Number.parseFloat(nextZoom.toFixed(2)));
-  };
-
   return (
     <div>
-      <div ref={mapContainer!} class={styles.mapContainer}>
-      <div class={styles.zoomContainer}>
-        <label class={styles.zoomLabel} htmlFor='zoom-input'>Zoom: </label>
-        <input class={styles.zoomInput} name='zoom-input' id='zoom-input' type="number" value={zoom()} min={1} max={14} step={.25} onChange={changeZoom}/>
-      </div>
-      </div>
+      <div ref={mapContainer!} class={styles.mapContainer}/>
     </div>
   )};
 
