4605cc64151178a8cdc7decd67147b2dc6ecc163
timothy <miller.tim108@gmail.com>
2022-05-14 10:54:31 -0400

    init gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/solid_ts-maplibre-nyc_boroughs',
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
